559abcde4f104e0ab0925ca4c9b3a147e0e4b8a1
Khushal Singh <98296678+khushalsing814@users.noreply.github.com>
2023-08-28 23:01:49 +0530

    update design add video

--- src/components/header.js
+++ src/components/header.js
@@ -5,14 +5,17 @@ function Header() {
         <>
             <header className='background_image_header'>
                 <div className='parent_image'>
-                    <img src='https://reactjs-ecommerce-app.vercel.app/assets/main.png.jpg'></img>
-                </div>
-                <p className='container header_text'><h1>New Season Arrivals</h1>
-                    This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </header>
+                    {/* <img src='https://reactjs-ecommerce-app.vercel.app/assets/main.png.jpg'></img> */}
+                    <video loop muted autoPlay="autoplay" style={{height:"100%" ,width:"100%"}}>
+                        <source src="./video/womanvideo.mp4" type="video/mp4"/>
+                       </video>
+                        </div>
+                        {/* <p className='container header_text' style={{color:"black"}}><h1>New Season Arrivals</h1>
+                            This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> */}
+                    </header>
 
-        </>
-    )
+                </>
+                )
 }
 
-export default Header
+                export default Header
